a96f07b09025e40865fffd20181bee109484267f
JeremiahID <jeremiahefe.je@gmail.com>
2024-03-19 17:18:11 +0100

    mobile view port

--- index.js
+++ index.js
@@ -21,3 +21,42 @@ CloseHambuger.addEventListener('click', () =>{
         Navigation.style.display = "none";
     }
 })
+
+
+const Admission = document.querySelector('#admission');
+const AdmissionLinks = document.querySelector('#admissionLinks');
+const Academics = document.querySelector('#academics');
+const AcademicsLinks = document.querySelector('#academicsLinks');
+const SchoolPortal = document.querySelector('#schoolPortal');
+const SchoolPortalLinks = document.querySelector('#schoolPortalLink');
+
+Admission.addEventListener('click', () =>{
+    console.log('working')
+    if(AdmissionLinks.style.display === "none" ){
+        AdmissionLinks.style.display = "block";
+        AcademicsLinks.style.display = "none"
+    } else {
+        AdmissionLinks.style.display = "none";
+    }
+});
+
+Academics.addEventListener('click', () =>{
+    console.log('working')
+    if(AcademicsLinks.style.display === "none" ){
+        AcademicsLinks.style.display = "block";
+        AdmissionLinks.style.display = "none";
+    } else {
+        AcademicsLinks.style.display = "none";
+    }
+});
+
+
+
+SchoolPortal.addEventListener('click', ()=>{
+    console.log("working");
+    if(SchoolPortalLinks.style.display === "none" ){
+        SchoolPortalLinks.style.display = "block";
+    } else {
+        SchoolPortalLinks.style.display = "none";
+    }
+});
